aceae02e60aa43502e29c98d7ec799f1f3ff607f
Rafael Evangelista <Rafaelhugo03@outlook.com>
2022-04-01 16:57:31 -0300

    classe de contexto de dados adicionada

---
+++ DevJobs/Data/DataContext.cs
@@ -0,0 +1,14 @@
+﻿using DevJobs.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace DevJobs.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+        }
+        public DbSet<JobVacancy> JobVacancies { get; set; }
+        public DbSet<JobApplication> JobApplications { get; set;}
+    }
+}
