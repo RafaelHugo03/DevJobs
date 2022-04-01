18f7c3011f8122b97c6c9fa3801f183956d02d14
Rafael Evangelista <Rafaelhugo03@outlook.com>
2022-04-01 16:50:30 -0300

    Entidades adicionadas

---
+++ DevJobs/Entities/JobApplication.cs
@@ -0,0 +1,17 @@
+﻿namespace DevJobs.Entities
+{
+    public class JobApplication
+    {
+        public int Id { get; set; }
+        public string ApplicantName { get; set; }
+        public string ApplicantEmail { get; set; }
+        public int JobVacancyId { get; set; }
+
+        public JobApplication(string applicantName, string applicantEmail, int jobVacancyId)
+        {
+            ApplicantName = applicantName;
+            ApplicantEmail = applicantEmail;
+            JobVacancyId = jobVacancyId;
+        }
+    }
+}
---
+++ DevJobs/Entities/JobVacancy.cs
@@ -0,0 +1,32 @@
+﻿namespace DevJobs.Entities
+{
+    public class JobVacancy
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string  Company { get; set; }
+        public bool IsRemote { get; set; }
+        public string SalaryRamge { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public List<JobApplication> Applications { get; set; }
+
+        public JobVacancy(string title, string description, string company, bool isRemote, string salaryRamge)
+        {
+            Title = title;
+            Description = description;
+            Company = company;
+            IsRemote = isRemote;
+            SalaryRamge = salaryRamge;
+
+            CreatedAt = DateTime.Now;
+            Applications = new List<JobApplication>();
+        }
+
+        public void Update(string title, string description) 
+        {
+            Title = title;
+            Description = description;
+        }
+    }
+}
