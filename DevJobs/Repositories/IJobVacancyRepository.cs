8820a6c7e6a4ca614ea27e161b773d89f1322d0e
Rafael Evangelista <Rafaelhugo03@outlook.com>
2022-04-04 09:08:43 -0300

    JobVacancyRepository Adicionado

---
+++ DevJobs/Repositories/IJobVacancyRepository.cs
@@ -0,0 +1,13 @@
+﻿using DevJobs.Entities;
+
+namespace DevJobs.Repositories
+{
+    public interface IJobVacancyRepository
+    {
+        List<JobVacancy> GetAll();
+        JobVacancy GetById(int id);
+        void Create(JobVacancy jobVacancy);
+        void Update(JobVacancy jobVacancy);
+        void delete(int id);
+    }
+}
---
+++ DevJobs/Repositories/JobVacancyRepository.cs
@@ -0,0 +1,48 @@
+﻿using DevJobs.Data;
+using DevJobs.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace DevJobs.Repositories
+{
+    public class JobVacancyRepository : IJobVacancyRepository
+    {
+        private readonly DataContext _context;
+
+        public JobVacancyRepository(DataContext context)
+        {
+            _context = context;
+        }
+
+        public void Create(JobVacancy jobVacancy)
+        {
+            _context.Add(jobVacancy);
+            _context.SaveChanges();
+        }
+
+        public void delete(int id)
+        {
+            var toDelete = _context.JobVacancies.FirstOrDefault(x => x.Id == id);
+            _context.Remove(toDelete);
+            _context.SaveChanges();
+        }
+
+        public List<JobVacancy> GetAll()
+        {
+            return _context.JobVacancies.AsNoTracking().ToList();
+        }
+
+        public JobVacancy GetById(int id)
+        {
+            return _context.JobVacancies
+                .Include(x => x.Applications)
+                .AsNoTracking()
+                .FirstOrDefault(x => x.Id == id);
+        }
+
+        public void Update(JobVacancy jobVacancy)
+        {
+            _context.Update(jobVacancy);
+            _context.SaveChanges();
+        }
+    }
+}
